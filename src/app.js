eae34a346aaf10bbbc7c4c0c550db4f640d3e0dc
Antonio Borrero <antojoseabad@gmail.com>
2025-05-26 16:39:03 +0000

    prueba de array

--- src/app.js
+++ src/app.js
@@ -4,8 +4,15 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { jsx } from "react/jsx-runtime";
+
+const card = document.querySelector(".card");
+  const numbers = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = document.createElement("P");
+  number.innerHTML = numbers[2];
+  number.className = "number"
+  card.appendChild(number)
 };
